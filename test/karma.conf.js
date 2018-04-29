eed4c68ee5dccf8fafbcb6a88ecd7259fecad355
马金花儿 <o.o@mug.dog>
2018-04-29 08:48:05 +0800

    use lcov reporter

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,8 +25,7 @@ module.exports = function(config) {
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['progress', 'coverage'],
         coverageReporter: {
-            type : 'html',
-            dir : 'coverage/'
+            reporters: [{type: 'lcov'}]
         },
         // web server port
         port: 9876,
